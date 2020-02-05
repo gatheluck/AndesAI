d6d316cb85d198c1c93a9ab9e0cc1b03f9d2172e
gatheluck <gatheluck+tech@gmail.com>
2020-02-05 13:19:07 +0900

    fix index order

--- andesai/transforms/fourier_noise.py
+++ andesai/transforms/fourier_noise.py
@@ -10,6 +10,12 @@ import torchvision
 
 class FourierNoise(object):
     def __init__(self, index_h:int, index_w:int, eps:float):
+        """
+        Args
+            index_h: index of fourier basis about hight direction 
+            index_w: index of fourier basis about width direction 
+            eps: size of noise(perturbation)
+        """
         assert abs(index_h)>=1 and abs(index_w)>=1
         assert eps>0.0
 
@@ -21,15 +27,17 @@ class FourierNoise(object):
         c,h,w = x.shape[-3:]
         if h%2!=0 or w%2!=0:
             raise ValueError('shape of x is invalid')
-
-        h_center = int(h/2)-1
-        w_center = int(w/2)-1
+        assert -h/2<=self.index_h<=h/2
+        assert -w/2<=self.index_w<=w/2
 
         w_real = torch.zeros_like(x[0,:,:])
         w_imaginary = torch.zeros_like(w_real)
 
-        w_real[h_center+self.index_h, w_center+self.index_w] = 1.0
-        w_real[h_center-self.index_h+1, w_center-self.index_w+1] = 1.0
+        # make low frequency-centered Fourier palne
+        # default Fourier palne is high frequency-centered
+        # so swap 1st and 3rd quadrant and swap 2nd and 4th quadrant        
+        w_real[self.index_h-1, self.index_w-1] = 1.0
+        w_real[h-self.index_h, w-self.index_w] = 1.0
 
         w = torch.stack([w_real, w_imaginary], dim=-1)
 
@@ -49,9 +57,9 @@ if __name__ == '__main__':
     #torch.multiprocessing.set_start_method('spawn')
     from andesai.data import DatasetBuilder
 
-    optional_transform = [FourierNoise(16,16,4.0)]
+    optional_transform = [FourierNoise(2,2,4.0)]
 
-    dataset_builder = DatasetBuilder(name='cifar10', root_path='../../data')
+    dataset_builder = DatasetBuilder(name='imagenet100', root_path='../../data')
     test_dataset   = dataset_builder(train=False, normalize=False, optional_transform=optional_transform)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True, num_workers=8, pin_memory=True)
     
